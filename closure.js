97b05d6550cb9b8f6718083dd338e1f3f5e85a92
tithi23 <tithiratry@gmail.com>
2021-08-31 06:52:42 -0700

    some interesting problem solutions

---
+++ closure.js
@@ -0,0 +1,17 @@
+function stopWatch() {
+    let counter = 0;
+    return function () {
+        counter++;
+        return counter;
+    }
+}
+let clock1 = stopWatch();
+console.log(clock1());
+console.log(clock1());
+
+
+let clock2 = stopWatch();
+console.log(clock2());
+
+let clock3 = stopWatch();
+console.log(clock3());
---
+++ data-types.js
@@ -0,0 +1,7 @@
+// let a = 19;
+// console.log(typeof a);
+
+// let a = 'Jamal Uddin Shahen Shah';
+
+let a = false;
+console.log(typeof a);
